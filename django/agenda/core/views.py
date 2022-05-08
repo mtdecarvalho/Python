1812f3fc328da834a7031ed079a89ebc0fd183c3
mtdecarvalho <matheusdecarvalhorr@gmail.com>
2022-05-08 11:09:19 -0300

    Inserção de eventos na agenda

--- django/agenda/core/views.py
+++ django/agenda/core/views.py
@@ -23,7 +23,7 @@ def submit_login(request):
     if request.POST:
         username = request.POST.get("username")
         password = request.POST.get("password")
-        usuario = authenticate(username=username,password=password)
+        usuario = authenticate(username=username, password=password)
         if usuario is not None:
             login(request, usuario)
             return redirect('/')
@@ -33,8 +33,27 @@ def submit_login(request):
 
 
 @login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get("titulo")
+        data_evento = request.POST.get("data_evento")
+        descricao = request.POST.get("descricao")
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
