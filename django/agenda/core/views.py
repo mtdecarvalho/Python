3ff4c71e6872ccc06835a366e43bd7e4a66030d8
mtdecarvalho <matheusdecarvalhorr@gmail.com>
2022-05-09 11:34:02 -0300

    Filtros, tipos de responses e configurações

--- django/agenda/core/views.py
+++ django/agenda/core/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, HttpResponse
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 
 # Create your views here.
@@ -64,16 +67,25 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+
     if (usuario == evento.usuario):
         evento.delete()
+    else:
+        raise Http404()
+
     return redirect('/')
 
 
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
@@ -89,3 +101,9 @@ def evento(request):
 
 def get_local_evento(request, titulo_evento):
     return HttpResponse(f'Local do evento {titulo_evento}: {Evento.objects.get(titulo=titulo_evento).local}')
+
+
+def json_lista_evento(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
