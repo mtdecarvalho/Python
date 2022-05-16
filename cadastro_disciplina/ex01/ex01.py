d65bdd21efcdbd5cc1859f03908ade20a163de36
mtdecarvalho <matheusdecarvalhorr@gmail.com>
2022-05-16 15:25:35 -0300

    Iniciado o port de alguns exercícios de C da faculdade para Python

---
+++ cadastro_disciplina/ex01/ex01.py
@@ -0,0 +1,39 @@
+class Disciplina:
+    def __init__(self, codigo, nome, professor, creditos, ano, semestre, nota1, nota2, media):
+        self.codigo = codigo
+        self.nome = nome
+        self.professor = professor
+        self.creditos = creditos
+        self.ano = ano
+        self.semestre = semestre
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.media = (self.nota1 + 2.0*self.nota2)/3.0
+
+    def __str__(self):
+        return  "Codigo    : {0:0>4}\n"\
+                "Nome      : {1}\n"\
+                "Professor : {2}\n"\
+                "Creditos  : {3}\n"\
+                "Ano       : {4}\n"\
+                "Semestre  : {5}\n"\
+                "Nota1     : {6:.2f}\n"\
+                "Nota2     : {7:.2f}\n"\
+                "Media     : {8:.2f}\n".format(
+                    self.codigo, self.nome, self.professor,
+                    self.creditos, self.ano, self.semestre,
+                    self.nota1, self.nota2, self.media
+                )
+
+if __name__ == "__main__":
+    codigo = int(input())
+    nome = input()
+    professor = input()
+    creditos = int(input())
+    ano = int(input())
+    semestre = int(input())
+    nota1 = float(input())
+    nota2 = float(input())
+    media = (nota1 + 2.0*nota2)/3.0
+    disciplina = Disciplina(codigo, nome, professor, creditos, ano, semestre, nota1, nota2, media)
+    print(disciplina)
